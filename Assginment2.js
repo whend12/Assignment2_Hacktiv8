985e64981ed46ed26d4ca1a4866812eac3bb7692
Wendy Gardiel <wendygardiel@gmail.com>
2023-09-07 21:34:47 +0700

    Add validasi if the form is not filled then the value is still there

--- Assginment2.js
+++ Assginment2.js
@@ -1,19 +1,53 @@
-function Portofolio() {
-  var Nama = document.getElementById("inputnama").value;
-  var role = document.getElementById("inputrole").value;
-  var Avaibility = document.getElementById("inputavaibility").value;
-  var Usia = document.getElementById("inputusia").value;
-  var Lokasi = document.getElementById("inputlokasi").value;
-  var Experience = document.getElementById("inputyearsexperience").value;
-  var Email = document.getElementById("inputemail").value;
+function getInputValue(elementId) {
+  return document.getElementById(elementId).value;
+}
 
-  document.getElementById("nama").innerHTML = Nama;
-  document.getElementById("role").innerHTML = role;
-  document.getElementById("Avaibility").innerHTML = Avaibility;
-  document.getElementById("usia").innerHTML = Usia;
-  document.getElementById("lokasi").innerHTML = Lokasi;
-  document.getElementById("experience").innerHTML = Experience;
-  document.getElementById("email").innerHTML = Email;
+function Portofolio() {
+  var Nama = getInputValue("inputnama");
+  var role = getInputValue("inputrole");
+  var Avaibility = getInputValue("inputavaibility");
+  var Usia = getInputValue("inputusia");
+  var Lokasi = getInputValue("inputlokasi");
+  var Experience = getInputValue("inputyearsexperience");
+  var Email = getInputValue("inputemail");
+
+  // Validasi agar jika form tidak di isi maka default tidak hilang
+  if (Nama) {
+    document.getElementById("nama").innerHTML = Nama;
+  }
+  if (role) {
+    document.getElementById("role").innerHTML = role;
+  }
+  if (Avaibility) {
+    document.getElementById("Avaibility").innerHTML = Avaibility;
+  }
+  if (Usia) {
+    document.getElementById("usia").innerHTML = Usia;
+  }
+  if (Lokasi) {
+    document.getElementById("lokasi").innerHTML = Lokasi;
+  }
+  if (Experience) {
+    document.getElementById("experience").innerHTML = Experience;
+  }
+  if (Email) {
+    document.getElementById("email").innerHTML = Email;
+  }
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 
   return false;
 }
+
+// Action untuk Edit
+var editButton = document.getElementById("editButton");
+
+var form = document.querySelector(".form");
+
+editButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  form.scrollIntoView({ behavior: "smooth" });
+});
